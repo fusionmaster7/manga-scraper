824cf8b89bdf8099b3151ecb6ea6c14ac3a3bd2b
Hardik Singh <hardiksingh297@gmail.com>
2020-06-05 13:29:43 +0530

    Extracted links for a single chapter

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import requests
-import pprint
+from bs4 import BeautifulSoup
 
-URL = 'https://manganelo.com/manga/read_one_piece_manga_online_free4'
-page = requests.get(URL)
+URL = 'https://mangakakalot.com/manga/hw922220'
+manga_page = requests.get(URL)
 
-print(page.content)
+soup = BeautifulSoup(manga_page.content, 'html.parser')
+chapter_results = soup.find('div', class_='chapter-list')
+
+chapter_url = chapter_results.a['href']
+chapter = requests.get(chapter_url)
+chapter_page = BeautifulSoup(chapter.content, 'html.parser')
+
+image_results = chapter_page.find('div', class_='vung-doc')
+image_links = image_results.find_all('img')
+images = []
+for image in image_links:
+    images.append(image['src'])
+print(images)
